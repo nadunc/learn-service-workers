0248580cc5aeb845b6788c4f55e984bf4c59574f
Nadun Chamikara <djnadunc@gmail.com>
2019-05-06 17:49:58 +0530

    service worker update

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,12 @@
 var CACHE_NAME = 'learn-service-workers-cache-v1';
 var urlsToCache = [
-    'https://nadunc.github.io/learn-service-workers/',
-    'https://nadunc.github.io/learn-service-workers/index.html',
-    'https://nadunc.github.io/learn-service-workers/assets/css/bootstrap.min.css',
-    'https://nadunc.github.io/learn-service-workers/assets/js/jquery-3.4.0.min.js',
-    'https://nadunc.github.io/learn-service-workers/assets/js/popper.min.js',
-    'https://nadunc.github.io/learn-service-workers/assets/js/bootstrap.min.js'
+    '',
+    'index.html',
+    'assets/css/bootstrap.min.css',
+    'assets/js/jquery-3.4.0.min.js',
+    'assets/js/popper.min.js',
+    'assets/js/bootstrap.min.js',
+    'service-worker.js'
 ];
 
 self.addEventListener('install', function (event) {
@@ -35,7 +36,7 @@ self.addEventListener('fetch', function (event) {
 
 self.addEventListener('activate', function(event) {
 
-    var cacheWhitelist = ['learn-service-workers-cache-v1'];
+    var cacheWhitelist = ['learn-service-workers-cache-v2'];
 
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
